f0d8fdfb398f3cd3c25aa8ebf166d33c4a6c7a1f
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-01-24 18:22:33 +0600

    created and added properties to the model class named 'Employee'

---
+++ EmployeeManagement/Models/Employee.cs
@@ -0,0 +1,9 @@
+namespace EmployeeManagement.Models;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Department { get; set; }
+}
