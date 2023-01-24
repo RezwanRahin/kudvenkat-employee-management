988da7ddbba729832c64fa7c0024037e1ef8ffc6
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-01-24 12:59:19 +0600

    added services to the dependency injection container and added serveral middlewire, for MVC setup, in project Program file

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -1,8 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+// app.MapGet("/", () => "Hello World!");
+
+app.UseHttpsRedirection();
 
 app.UseStaticFiles();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
