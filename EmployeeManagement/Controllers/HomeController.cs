498b36bb17ac5e05f7dec486a242e0b3394bd64c
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-01-24 18:28:42 +0600

    added a property and a contructor in 'HomeController' class

--- EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -1,9 +1,17 @@
+using EmployeeManagement.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeManagement.Controllers;
 
 public class HomeController : Controller
 {
+    private IEmployeeRepository _employeeRepository;
+    
+    public HomeController(IEmployeeRepository employeeRepository)
+    {
+        _employeeRepository = employeeRepository;
+    }
+    
     public JsonResult Index()
     {
         return Json(new { id = 1, name = "dotnet" });
