5545e9c025155fea5ed2428fa86a3bd3d3a365b0
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-01-25 13:46:42 +0600

    demo of 'ViewData' for Detail view in HomeController

--- EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -20,6 +20,8 @@ public class HomeController : Controller
     public ViewResult Details()
     {
         Employee model = _employeeRepository.GetEmployee(1);
+        ViewData["Employee"] = model;
+        ViewData["PageTitle"] = "Employee Details";
         return View();
     }
 }
