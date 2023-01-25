461b2a504bb6c90008882ff21efab1a1f6540f2d
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-01-25 19:55:16 +0600

    created a ViewModel, adding properties, named 'HomeDetailsViewModel'

---
+++ EmployeeManagement/ViewModels/HomeDetailsViewModel.cs
@@ -0,0 +1,9 @@
+using EmployeeManagement.Models;
+
+namespace EmployeeManagement.ViewModels;
+
+public class HomeDetailsViewModel
+{
+    public Employee Employee { get; set; }
+    public string PageTitle { get; set; }
+}
