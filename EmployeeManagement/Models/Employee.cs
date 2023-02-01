e6051a336c466eaa3cc11c4512bc1c3abecb4508
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-02-01 19:23:10 +0600

    changed the 'Department' property type to 'Dept' in 'Employee' model class

--- EmployeeManagement/Models/Employee.cs
+++ EmployeeManagement/Models/Employee.cs
@@ -5,5 +5,5 @@ public class Employee
     public int Id { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
-    public string Department { get; set; }
+    public Dept Department { get; set; }
 }
