3cca3e949889cedb5b749e1e0a6547400e6cb421
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-02-02 17:57:47 +0600

    implemented a method named 'Add' in MockEmployeeRepository class

--- EmployeeManagement/Models/MockEmployeeRepository.cs
+++ EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -19,6 +19,13 @@ public class MockEmployeeRepository : IEmployeeRepository
         return _employeeList;
     }
 
+    public Employee Add(Employee employee)
+    {
+        employee.Id = _employeeList.Max(e => e.Id) + 1;
+        _employeeList.Add(employee);
+        return employee;
+    }
+
     public Employee GetEmployee(int Id)
     {
         return _employeeList.FirstOrDefault(e => e.Id == Id);
