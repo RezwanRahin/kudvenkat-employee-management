7e3c82c4ba72907871ae28a1b5b7f3476f284112
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-02-03 20:48:48 +0600

    for demo of 'AddScoped' service, modifed the project Program file

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
+builder.Services.AddScoped<IEmployeeRepository, MockEmployeeRepository>();
 
 var app = builder.Build();
 
