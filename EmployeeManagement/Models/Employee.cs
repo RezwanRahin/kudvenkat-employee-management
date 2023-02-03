c33a2d62c9f8b31e3e8be0756c11cae4dce3e1bb
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-02-03 18:21:56 +0600

    attached 'required' attribute to 'Name' and 'Email' properties in 'Employee' model class

--- EmployeeManagement/Models/Employee.cs
+++ EmployeeManagement/Models/Employee.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace EmployeeManagement.Models;
 
 public class Employee
 {
     public int Id { get; set; }
+    [Required]
     public string Name { get; set; }
+    [Required]
     public string Email { get; set; }
     public Dept Department { get; set; }
 }
