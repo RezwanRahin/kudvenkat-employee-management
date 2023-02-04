5453166c171d786e1be22bc4a617fd8d81881604
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-02-04 19:13:54 +0600

    added abstract methods 'Update' and 'Delete' in 'IEmployeeRepository' class

--- EmployeeManagement/Models/IEmployeeRepository.cs
+++ EmployeeManagement/Models/IEmployeeRepository.cs
@@ -5,4 +5,6 @@ public interface IEmployeeRepository
     Employee GetEmployee(int Id);
     IEnumerable<Employee> GetAllEmployees();
     Employee Add(Employee employee);
+    Employee Update(Employee employeeChanges);
+    Employee Delete(int Id);
 }
