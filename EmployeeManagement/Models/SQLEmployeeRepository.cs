540f8bfbf9ecf35c5a4ff901f3fad816a4734ae5
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-02-04 20:15:29 +0600

    created a new class, implementing 'IEmployeeRepository' interface, named 'SQLEmployeeRepository'

---
+++ EmployeeManagement/Models/SQLEmployeeRepository.cs
@@ -0,0 +1,29 @@
+namespace EmployeeManagement.Models;
+
+public class SQLEmployeeRepository : IEmployeeRepository
+{
+    public Employee GetEmployee(int Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Employee> GetAllEmployees()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Employee Add(Employee employee)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Employee Update(Employee employeeChanges)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Employee Delete(int Id)
+    {
+        throw new NotImplementedException();
+    }
+}
