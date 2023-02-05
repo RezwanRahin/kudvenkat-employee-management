051134c185ed6ba195933bc6a92a9dfab75477b3
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-02-05 16:26:02 +0600

    added another migration named 'AlterEmployeesSeedData'

---
+++ EmployeeManagement/Migrations/20230205102318_AlterEmployeesSeedData.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeManagement.Migrations
+{
+    public partial class AlterEmployeesSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Email", "Name" },
+                values: new object[] { "mary@g.com", "Mary" });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Department", "Email", "Name" },
+                values: new object[] { 2, 2, "john@g.com", "John" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Email", "Name" },
+                values: new object[] { "mark@g.com", "Mark" });
+        }
+    }
+}
