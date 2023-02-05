47baeea1f8d92fdb24b8b14e05ddad916c1b8bed
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-02-05 17:05:36 +0600

    created class, as ModelBuilderExtension, and added seed data, named 'ModelBuilderExtensions'

---
+++ EmployeeManagement/Models/ModelBuilderExtensions.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Models;
+
+public static class ModelBuilderExtensions
+{
+    public static void Seed(this ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Employee>().HasData(
+            new Employee
+            {
+                Id = 1,
+                Name = "Mary",
+                Department = Dept.IT,
+                Email = "mary@g.com"
+            },
+            new Employee
+            {
+                Id = 2,
+                Name = "John",
+                Department = Dept.IT,
+                Email = "john@g.com"
+            }
+        );
+    }
+}
