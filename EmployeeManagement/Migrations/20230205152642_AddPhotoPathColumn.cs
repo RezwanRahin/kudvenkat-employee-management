22c0839670cebe78408009ca2f0551b58001b610
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-02-06 19:20:53 +0600

    added another migration named 'AddPhotoPathColumn'

---
+++ EmployeeManagement/Migrations/20230205152642_AddPhotoPathColumn.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeManagement.Migrations
+{
+    public partial class AddPhotoPathColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoPath",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PhotoPath",
+                value: "path1");
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "PhotoPath",
+                value: "path2");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhotoPath",
+                table: "Employees");
+        }
+    }
+}
