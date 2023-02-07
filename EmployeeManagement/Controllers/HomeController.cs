b41cf61da0e01c902743d31529dd2f2b0b6641bf
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-02-07 14:27:48 +0600

    modified the post 'Create' action method, for uploading image file with 'EmployeeCreateViewModel' model, in 'HomeController' class

--- EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -40,12 +40,29 @@ public class HomeController : Controller
     }
 
     [HttpPost]
-    public IActionResult Create(Employee employee)
+    public IActionResult Create(EmployeeCreateViewModel model)
     {
         if (ModelState.IsValid)
         {
-            Employee newEmployee = _employeeRepository.Add(employee);
-            // return RedirectToAction("Details", new { id = newEmployee.Id });
+            string uniqueFileName = null;
+            if (model.Photo != null)
+            {
+                string uploadsFolder = Path.Combine(_hostingEnvironment.WebRootPath, "images");
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Photo.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                model.Photo.CopyTo(new FileStream(filePath, FileMode.Create));
+            }
+
+            Employee newEmployee = new Employee()
+            {
+                Name = model.Name,
+                Email = model.Email,
+                Department = model.Department,
+                PhotoPath = uniqueFileName
+            };
+
+            _employeeRepository.Add(newEmployee);
+            return RedirectToAction("Details", new { id = newEmployee.Id });
         }
         
         return View();
