3badeedbf252854f55d7b38bd00fbc101e4e4bc1
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-02-08 21:34:20 +0600

    added 2 properties to 'EmployeeEditViewModel' class

--- EmployeeManagement/ViewModels/EmployeeEditViewModel.cs
+++ EmployeeManagement/ViewModels/EmployeeEditViewModel.cs
@@ -2,5 +2,6 @@ namespace EmployeeManagement.ViewModels;
 
 public class EmployeeEditViewModel : EmployeeCreateViewModel
 {
-    
+    public int Id { get; set; }
+    public string ExistingPhotoPath { get; set; }
 }
