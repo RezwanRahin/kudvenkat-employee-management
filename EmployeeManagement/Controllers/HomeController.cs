09c8739a840df1df9ba2ab5eea4340168c6ff07f
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-02-12 12:42:36 +0600

    modified the 'Details' action method, for handling '404 not found' for employee id, in 'HomeController' class

--- EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -24,9 +24,17 @@ public class HomeController : Controller
 
     public ViewResult Details(int? id)
     {
+        Employee employee = _employeeRepository.GetEmployee(id.Value);
+
+        if (employee == null)
+        {
+            Response.StatusCode = 404;
+            return View("EmployeeNotFound", id.Value);
+        }
+
         HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel()
         {
-            Employee = _employeeRepository.GetEmployee(id??1),
+            Employee = employee,
             PageTitle = "Employee Details"
         };
 
