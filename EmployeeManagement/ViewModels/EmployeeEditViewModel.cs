973848a7d833ecea1871133eb89f7044b66b1535
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-02-12 19:11:58 +0600

    made 'ExistingPhotoPath' property nullable in 'EmployeeEditViewModel' class

--- EmployeeManagement/ViewModels/EmployeeEditViewModel.cs
+++ EmployeeManagement/ViewModels/EmployeeEditViewModel.cs
@@ -3,5 +3,5 @@ namespace EmployeeManagement.ViewModels;
 public class EmployeeEditViewModel : EmployeeCreateViewModel
 {
     public int Id { get; set; }
-    public string ExistingPhotoPath { get; set; }
+    public string? ExistingPhotoPath { get; set; }
 }
