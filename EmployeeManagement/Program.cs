77d3be4d31cf5de39f94b47fd3d6f417b126eaba
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-02-14 18:32:25 +0600

    demo of 'UseStatusCodePagesWithReExecute' in project Program file

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -18,7 +18,8 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     // app.UseExceptionHandler("/Home/Error");
-    app.UseStatusCodePagesWithRedirects("/Error/{0}");
+    // app.UseStatusCodePagesWithRedirects("/Error/{0}");
+    app.UseStatusCodePagesWithReExecute("/Error/{0}");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
