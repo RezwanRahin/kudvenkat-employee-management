13c7ca8227ef18a38c8fc3d1cd3ed33f36a642f7
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-02-15 17:41:00 +0600

    modifed 'HttpStatusCodeHandler' action method, for providing 'Status Code' info to the corrosponding view, in 'ErrorController' class

--- EmployeeManagement/Controllers/ErrorController.cs
+++ EmployeeManagement/Controllers/ErrorController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeManagement.Controllers;
@@ -7,10 +8,14 @@ public class ErrorController : Controller
     [Route("Error/{statusCode}")]
     public IActionResult HttpStatusCodeHandler(int statusCode)
     {
+        var statusCodeResult = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
+
         switch (statusCode)
         {
             case 404:
                 ViewBag.ErrorMessage = "Sorry, the rosource you requested could not be found";
+                ViewBag.Path = statusCodeResult.OriginalPath;
+                ViewBag.QS = statusCodeResult.OriginalQueryString;
                 break;
         }
 
