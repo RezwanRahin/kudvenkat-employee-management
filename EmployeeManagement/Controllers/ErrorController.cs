5a1750e6b09da906163770d416b916c6dd0e0c25
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-02-16 15:25:33 +0600

    added an action method named 'Error' in 'ErrorController' class

--- EmployeeManagement/Controllers/ErrorController.cs
+++ EmployeeManagement/Controllers/ErrorController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 
@@ -20,5 +21,18 @@ public class ErrorController : Controller
         }
 
         return View("NotFound");
+    }
+
+    [Route("Error")]
+    [AllowAnonymous]
+    public IActionResult Error()
+    {
+        var exceptionDetails = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+
+        ViewBag.ExceptionPath = exceptionDetails.Path;
+        ViewBag.ExceptionMessage = exceptionDetails.Error.Message;
+        ViewBag.stacktrace = exceptionDetails.Error.StackTrace;
+
+        return View("Error");
     }
 }
