cbf933ea9c338c3529509801b68f4839f10b8f05
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-02-16 15:28:33 +0600

    added 'UseExceptionHandler' middleware in project Program file

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -19,6 +19,7 @@ if (!app.Environment.IsDevelopment())
 {
     // app.UseExceptionHandler("/Home/Error");
     // app.UseStatusCodePagesWithRedirects("/Error/{0}");
+    app.UseExceptionHandler("/Error");
     app.UseStatusCodePagesWithReExecute("/Error/{0}");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
