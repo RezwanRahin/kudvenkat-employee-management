3ad2ffe0b9781934c75a15203331ddfd89c9b75b
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-02-17 22:12:59 +0600

    made 'AppDbContext' class inherit 'IdentityDbContext' replacing 'DbContext' class

--- EmployeeManagement/Models/AppDbContext.cs
+++ EmployeeManagement/Models/AppDbContext.cs
@@ -1,8 +1,9 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace EmployeeManagement.Models;
 
-public class AppDbContext : DbContext
+public class AppDbContext : IdentityDbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
