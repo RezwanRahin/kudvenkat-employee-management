575b02d059019af7c4b795d439f5e0b360523939
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-02-18 18:44:09 +0600

    added constructor and injected 'UserManager' and 'SignInManager' dependency, via constructor, in 'AccountController' class

--- EmployeeManagement/Controllers/AccountController.cs
+++ EmployeeManagement/Controllers/AccountController.cs
@@ -1,9 +1,20 @@
+using EmployeeManagement.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeManagement.Controllers;
 
 public class AccountController : Controller
 {
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly SignInManager<IdentityUser> _signInManager;
+
+    public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+    
     [HttpGet]
     public IActionResult Register()
     {
