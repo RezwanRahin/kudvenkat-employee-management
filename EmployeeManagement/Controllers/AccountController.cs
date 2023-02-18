085c32f22e72d9f196366fa54f75d27e6add3ab8
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-02-18 21:06:16 +0600

    added a HttpPost action method, for registering users, named 'Register' in 'AccountController' class

--- EmployeeManagement/Controllers/AccountController.cs
+++ EmployeeManagement/Controllers/AccountController.cs
@@ -20,4 +20,27 @@ public class AccountController : Controller
     {
         return View();
     }
+
+    [HttpPost]
+    public async Task<IActionResult> Register(RegisterViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var user = new IdentityUser { UserName = model.Email, Email = model.Email };
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, isPersistent: false);
+                return RedirectToAction("Index", "Home");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
+
+        return View(model);
+    }
 }
