81b0a76186676031e481d74211930b441258dafa
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-02-18 07:13:41 +0600

    created a new controller, and added an action method named 'Register', named 'AccountController'

---
+++ EmployeeManagement/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers;
+
+public class AccountController : Controller
+{
+    [HttpGet]
+    public IActionResult Register()
+    {
+        return View();
+    }
+}
