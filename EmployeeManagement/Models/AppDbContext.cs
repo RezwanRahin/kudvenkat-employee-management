0053522d84667bdecce10d13d0ef765694f1b6cc
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-02-18 06:13:11 +0600

    modified the 'OnModelCreating' method, calling the base class method, of 'AppDbContext' class

--- EmployeeManagement/Models/AppDbContext.cs
+++ EmployeeManagement/Models/AppDbContext.cs
@@ -14,6 +14,7 @@ public class AppDbContext : IdentityDbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
         modelBuilder.Seed();
     }
 }
