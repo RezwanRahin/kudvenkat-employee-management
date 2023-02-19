408f4573a5de52f2c27154851670cae4a67937ce
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-02-19 16:06:25 +0600

    added a HttpPost action method named 'Login' in 'AccountController' class

--- EmployeeManagement/Controllers/AccountController.cs
+++ EmployeeManagement/Controllers/AccountController.cs
@@ -56,4 +56,22 @@ public class AccountController : Controller
     {
         return View();
     }
+
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
+        }
+
+        return View(model);
+    }
 }
