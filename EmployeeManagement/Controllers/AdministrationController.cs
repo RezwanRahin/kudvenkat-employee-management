85b26074bc6301eef4cbf4a8e3ab86e3167c0daf
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-02-21 19:40:48 +0600

    created a controller, and added constructor while injecting 'RoleManager' service via contructor injection, named 'AdministrationController'

---
+++ EmployeeManagement/Controllers/AdministrationController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers;
+
+public class AdministrationController : Controller
+{
+    private readonly RoleManager<IdentityRole> _roleManager;
+
+    public AdministrationController(RoleManager<IdentityRole> roleManager)
+    {
+        _roleManager = roleManager;
+    }
+}
