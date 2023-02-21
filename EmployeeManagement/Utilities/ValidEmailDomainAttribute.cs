ac76d06151bd053188b1aa1e38e51ca23f96a646
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-02-21 14:29:49 +0600

    added contructor along with a private field in 'ValidEmailDomainAttribute' class

--- EmployeeManagement/Utilities/ValidEmailDomainAttribute.cs
+++ EmployeeManagement/Utilities/ValidEmailDomainAttribute.cs
@@ -4,5 +4,10 @@ namespace EmployeeManagement.Utilities;
 
 public class ValidEmailDomainAttribute : ValidationAttribute
 {
-    
+    private readonly string _allowedDomain;
+
+    public ValidEmailDomainAttribute(string allowedDomain)
+    {
+        _allowedDomain = allowedDomain;
+    }
 }
