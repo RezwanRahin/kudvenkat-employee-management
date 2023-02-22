8c9a89d8bc8bdd8f98a456e9ec4135d47b4787c5
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-02-22 15:09:01 +0600

    create a ViewModel, and added a property, named 'CreateRoleViewModel'

---
+++ EmployeeManagement/ViewModels/CreateRoleViewModel.cs
@@ -0,0 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.ViewModels;
+
+public class CreateRoleViewModel
+{
+    [Required]
+    public string? RoleName { get; set; }
+}
