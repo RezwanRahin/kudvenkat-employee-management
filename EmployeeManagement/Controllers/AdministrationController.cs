0b6b3692cb2e80dfca5865bfc7e9cc2ca67d3f68
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-02-23 19:09:28 +0600

    added a HttpGet action method named 'EditRole' in 'AdministrationController' class

--- EmployeeManagement/Controllers/AdministrationController.cs
+++ EmployeeManagement/Controllers/AdministrationController.cs
@@ -50,4 +50,28 @@ public class AdministrationController : Controller
         var roles = _roleManager.Roles;
         return View(roles);
     }
+
+    [HttpGet]
+    public async Task<IActionResult> EditRole(string id)
+    {
+        var role = await _roleManager.FindByIdAsync(id);
+
+        if (role == null)
+        {
+            ViewBag.ErrorMessage = $"Role with Id = {id} cannot be found";
+            return View("NotFound");
+        }
+
+        var model = new EditRoleViewModel{ Id = role.Id, RoleName = role.Name };
+
+        foreach (var user in _userManager.Users)
+        {
+            if (await _userManager.IsInRoleAsync(user, role.Name))
+            {
+                model.Users.Add(user.UserName);
+            }
+        }
+
+        return View(model);
+    }
 }
