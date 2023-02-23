d69c3e43aef5ad57b14e3ab833f7a3e075be8271
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-02-23 19:07:13 +0600

    created a ViewModel, for editing role,  named 'EditRoleViewModel'

---
+++ EmployeeManagement/ViewModels/EditRoleViewModel.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.ViewModels;
+
+public class EditRoleViewModel
+{
+    public EditRoleViewModel()
+    {
+        Users = new List<string>();
+    }
+    
+    public string Id { get; set; }
+
+    [Required]
+    public string RoleName { get; set; }  
+
+    public List<string> Users { get; set; }  
+}
