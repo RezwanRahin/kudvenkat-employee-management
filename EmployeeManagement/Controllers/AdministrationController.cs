6ee7b7be3459a38d07936d3d84f50a71b1390267
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-02-26 19:35:25 +0600

    added a HttpPost action method named 'DeleteRole' in 'AdministrationController' class

--- EmployeeManagement/Controllers/AdministrationController.cs
+++ EmployeeManagement/Controllers/AdministrationController.cs
@@ -275,4 +275,32 @@ public class AdministrationController : Controller
             return View("ListUsers");
         }
     }
+
+    [HttpPost]
+    public async Task<IActionResult> DeleteRole(string id)
+    {
+        var role = await _roleManager.FindByIdAsync(id);
+
+        if (role == null)
+        {
+            ViewBag.ErrorMessage = $"Role with Id = {id} cannot be found";
+            return View("NotFound");
+        }
+        else
+        {
+            var result = await _roleManager.DeleteAsync(role);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("ListRoles");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+
+            return View("ListRoles");
+        }
+    }
 }
