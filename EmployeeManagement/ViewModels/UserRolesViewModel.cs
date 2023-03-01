2b46e0a1952ced00bee409444128592c74b20d3e
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-03-01 18:37:07 +0600

    created a ViewModel, and added relevant properties, named 'UserRolesViewModel'

---
+++ EmployeeManagement/ViewModels/UserRolesViewModel.cs
@@ -0,0 +1,8 @@
+namespace EmployeeManagement.ViewModels;
+
+public class UserRolesViewModel
+{
+    public string RoleId { get; set; }
+    public string RoleName { get; set; }
+    public bool IsSelected { get; set; }
+}
