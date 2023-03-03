09472f789c2f6a55e0dacb118e059ce533a38a71
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-03-03 20:08:32 +0600

    decorated 'DeleteRole' action method with 'Authorize' attribute, for policy based authorization, in 'AdministrationController' class

--- EmployeeManagement/Controllers/AdministrationController.cs
+++ EmployeeManagement/Controllers/AdministrationController.cs
@@ -283,6 +283,7 @@ public class AdministrationController : Controller
     }
 
     [HttpPost]
+    [Authorize(Policy = "DeleteRolePolicy")]
     public async Task<IActionResult> DeleteRole(string id)
     {
         var role = await _roleManager.FindByIdAsync(id);
