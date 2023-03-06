f120649117c091ff0f5a141b9c268c7b83f43385
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-03-06 11:37:47 +0600

    provided implementation of 'HandleRequirementAsync' method in Security/CanEditOnlyOtherAdminRolesAndClaimsHandler class

--- EmployeeManagement/Security/CanEditOnlyOtherAdminRolesAndClaimsHandler.cs
+++ EmployeeManagement/Security/CanEditOnlyOtherAdminRolesAndClaimsHandler.cs
@@ -6,5 +6,24 @@ namespace EmployeeManagement.Security;
 
 public class CanEditOnlyOtherAdminRolesAndClaimsHandler : AuthorizationHandler<ManageAdminRolesAndClaimsRequirement>
 {
+    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ManageAdminRolesAndClaimsRequirement requirement)
+    {
+        var authFilterContext = context.Resource as AuthorizationFilterContext;
+        if (authFilterContext == null)
+        {
+            return Task.CompletedTask;
+        }
 
+        string loggedInAdminId = context.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value;
+        string adminIdBeingEdited = authFilterContext.HttpContext.Request.Query["userId"];
+
+        if (context.User.IsInRole("Admin") &&
+        context.User.HasClaim(claim => claim.Type == "Edit Role" && claim.Value == "true") &&
+        adminIdBeingEdited.ToLower() != loggedInAdminId.ToLower())
+        {
+            context.Succeed(requirement);
+        }
+
+        return Task.CompletedTask;
+    }
 }
