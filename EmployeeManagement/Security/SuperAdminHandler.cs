cfa1deb66cd11d87da14f44347063d92b1768353
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-03-06 13:27:20 +0600

    provided implementation of 'HandleRequirementAsync' method in Security/SuperAdminHandler class

--- EmployeeManagement/Security/SuperAdminHandler.cs
+++ EmployeeManagement/Security/SuperAdminHandler.cs
@@ -4,5 +4,13 @@ namespace EmployeeManagement.Security;
 
 public class SuperAdminHandler : AuthorizationHandler<ManageAdminRolesAndClaimsRequirement>
 {
-    
+    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ManageAdminRolesAndClaimsRequirement requirement)
+    {
+        if (context.User.IsInRole("Super Admin"))
+        {
+            context.Succeed(requirement);
+        }
+        
+        return Task.CompletedTask;
+    }
 }
